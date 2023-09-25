943b8eb33fbdfc3edb4a1360cd88152e2230865b
Bilal Amjad <69304697+mirza-bilal-amjad@users.noreply.github.com>
2023-09-25 19:09:17 +0500

    Final Commit

--- BoilerPlate/src/screens/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Startup } from './Startup/Startup';
-export { default as Example } from './Example/Example';
-export {default as MyShifts} from './MyShifts/MyShifts';
-export {default as AvailableShifts} from './AvailableShifts/AvailableShifts';
